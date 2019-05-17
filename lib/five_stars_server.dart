f62f4a20aa0dbe36d2d8a575a4de65d7bd47a4f5
kekland <kk.erzhan@gmail.com>
2019-05-17 11:06:00 +0600

    Initial commit

---
+++ lib/five_stars_server.dart
@@ -0,0 +1,11 @@
+/// five_stars_server
+///
+/// A Aqueduct web server.
+library five_stars_server;
+
+export 'dart:async';
+export 'dart:io';
+
+export 'package:aqueduct/aqueduct.dart';
+
+export 'channel.dart';
